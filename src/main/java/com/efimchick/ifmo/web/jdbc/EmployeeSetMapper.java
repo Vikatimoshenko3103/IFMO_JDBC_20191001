60845faf8e5c628dcb4ac989e0d64512ebc3c408
Vikatimoshenko3103 <lalala3103@mail.ru>
2019-12-16 22:17:14 +0300

    solved

--- src/main/java/com/efimchick/ifmo/web/jdbc/EmployeeSetMapper.java
+++ src/main/java/com/efimchick/ifmo/web/jdbc/EmployeeSetMapper.java
@@ -1,4 +1,75 @@
 package com.efimchick.ifmo.web.jdbc;
 
-public class EmployeeSetMapper {
+import com.efimchick.ifmo.web.jdbc.domain.Employee;
+import com.efimchick.ifmo.web.jdbc.domain.FullName;
+import com.efimchick.ifmo.web.jdbc.domain.Position;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.*;
+
+public class EmployeeSetMapper implements SetMapper<Set<Employee>> {
+
+    private EmployeeInfo GenerateEmployee(ResultSet resultSet) throws SQLException {
+        BigInteger managerId= BigInteger.valueOf(-1);
+
+        if (resultSet.getString("manager") != null){
+            managerId = BigInteger.valueOf(resultSet.getInt("manager"));
+        }
+        return new EmployeeInfo(
+                BigInteger.valueOf(resultSet.getInt("id")),
+                new FullName(
+                        resultSet.getString("firstName"),
+                        resultSet.getString("lastName"),
+                        resultSet.getString("middleName")
+                ),
+                Position.valueOf(resultSet.getString("position")),
+                LocalDate.parse(resultSet.getString("hireDate")),
+                resultSet.getBigDecimal("salary"),
+                managerId
+        );
+    }
+
+    public class EmployeeInfo extends Employee{
+
+        private final BigInteger managerId;
+
+        EmployeeInfo(BigInteger id, FullName fullName, Position position, LocalDate hired, BigDecimal salary, BigInteger managerId) {
+            super(id, fullName, position, hired, salary, null);
+            this.managerId = managerId;
+        }
+
+        Employee ToEmployee(Map<BigInteger, EmployeeInfo> data){
+            Employee manager = null;
+            if (data.get(managerId) != null){
+                manager = data.get(managerId).ToEmployee(data);
+            }
+            return new Employee(getId(),getFullName(),getPosition(),getHired(),getSalary(), manager);
+        }
+    }
+
+    @Override
+    public Set<Employee> mapSet(ResultSet resultSet) {
+        try {
+            Set<EmployeeInfo> data = new HashSet<>();
+            Map<BigInteger, EmployeeInfo> mapData = new HashMap<>();
+            while (resultSet.next()) {
+                EmployeeInfo employee = GenerateEmployee(resultSet);
+
+                data.add(employee);
+                mapData.put(employee.getId(), employee);
+            }
+            Set<Employee> set = new HashSet<>();
+            for (EmployeeInfo entry : data){
+                set.add(entry.ToEmployee(mapData));
+            }
+            return set;
+        }
+        catch (SQLException e) {
+            return null;
+        }
+    }
 }
--- src/main/java/com/efimchick/ifmo/web/jdbc/SetMapperFactory.java
+++ src/main/java/com/efimchick/ifmo/web/jdbc/SetMapperFactory.java
@@ -1,12 +1,12 @@
 package com.efimchick.ifmo.web.jdbc;
 
-import java.util.Set;
-
 import com.efimchick.ifmo.web.jdbc.domain.Employee;
+
+import java.util.Set;
 
 public class SetMapperFactory {
 
     public SetMapper<Set<Employee>> employeesSetMapper() {
-        throw new UnsupportedOperationException();
+        return new EmployeeSetMapper();
     }
 }
